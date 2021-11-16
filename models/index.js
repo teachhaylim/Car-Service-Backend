ff167600490ccf02b25ca89ed68c4093dea7c26d
ShadowGMR <teachhaylim12@gmail.com>
2021-11-16 08:49:38 +0700

    restart

--- models/index.js
+++
@@ -1,6 +0,0 @@
-export { default as User } from "./user.model";
-export { default as Category } from "./category.model";
-export { default as Shop } from "./shop.model";
-export { default as Service } from "./service.model";
-export { default as Rate } from "./rating.model";
-export { default as Appointment } from "./appointment.model";
--- services/index.js
+++
@@ -1,8 +0,0 @@
-export { default as userService } from "./user.service";
-export { default as authService } from "./auth.service";
-export { default as tokenService } from "./token.service";
-export { default as categoryService } from "./category.service";
-export { default as shopService } from "./shop.service";
-export { default as rateService } from "./rate.service";
-export { default as serviceService } from "./service.service";
-export { default as appointmentService } from "./appointment.service";
